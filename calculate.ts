e1f7f2481462bed0b1752a51998b26079aeba7e8
caijialinxx <caijialinxx@foxmail.com>
2019-07-07 20:32:51 +0800

    使用class改写（面向对象编程）

--- calculate.ts
+++ calculate.ts
@@ -1,107 +1,121 @@
-{
-  // 计算器容器
-  let calculatorBox = document.createElement('div')
-  calculatorBox.className = 'calculator'
-  document.body.appendChild(calculatorBox)
+class Calculator {
+  calculatorBox: HTMLDivElement
+  outputSpan: HTMLSpanElement
+  n1: string = ''
+  n2: string = ''
+  operator: string = ''
+  result: string = '0'
+  stepNum: string = ''
+  prevOperator: string = ''
 
-  // 结果显示区
-  let outputBox = document.createElement('div')
-  let outputSpan = document.createElement('span')
-  outputSpan.textContent = '0'
-  outputSpan.className = 'output-span'
-  outputBox.className = 'output-box'
-  outputBox.appendChild(outputSpan)
-  calculatorBox.appendChild(outputBox)
-
-  // 按钮区
-  function createButton(text) {
+  constructor() {
+    this.createCalculator()
+    this.createOutput()
+    this.createButtons()
+  }
+  createCalculator() {
+    this.calculatorBox = document.createElement('div')
+    this.calculatorBox.className = 'calculator'
+    document.body.appendChild(this.calculatorBox)
+  }
+  createOutput() {
+    let outputBox = document.createElement('div')
+    this.outputSpan = document.createElement('span')
+    this.outputSpan.textContent = '0'
+    this.outputSpan.className = 'output-span'
+    outputBox.className = 'output-box'
+    outputBox.appendChild(this.outputSpan)
+    this.calculatorBox.appendChild(outputBox)
+  }
+  createButtons() {
+    let keysRows = [
+      ['Clear', '+/-', '÷'],
+      ['7', '8', '9', '×'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '='],
+    ]
+    keysRows.forEach(keys => {
+      let rowBox = document.createElement('div')
+      rowBox.className = 'buttons-row'
+      keys.forEach(keyText => {
+        rowBox.appendChild(this.createButton(keyText))
+      })
+      this.calculatorBox.appendChild(rowBox)
+    })
+    this.listenButtonsEvent()
+  }
+  createButton(text: string) {
     let button = document.createElement('button')
     button.textContent = text
     button.className = `button button__${text}`
     return button
   }
-  let keysRows = [
-    ['Clear', '+/-', '÷'],
-    ['7', '8', '9', '×'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
-    ['0', '.', '='],
-  ]
-  keysRows.forEach(keys => {
-    let rowBox = document.createElement('div')
-    rowBox.className = 'buttons-row'
-    keys.forEach(keyText => {
-      rowBox.appendChild(createButton(keyText))
-    })
-    calculatorBox.appendChild(rowBox)
-  })
-
-  // 按钮显示操作
-  let n1 = '', n2 = '', operator = '', result = '0', stepNum = '', prevOperator = ''
-  calculatorBox.addEventListener('click', (e) => {
-    if (e.target instanceof HTMLButtonElement) {
-      let buttonVal = e.target.textContent
-      if ('1234567890.'.indexOf(buttonVal) > -1) {
-        // 当按数字键时，如果操作符存在，那么一定是对n2进行赋值
-        if (operator) {
-          n2 = getNewNumString(n2, buttonVal)
-          outputSpan.textContent = n2
-        } else {
-          n1 = getNewNumString(n1, buttonVal)
-          outputSpan.textContent = n1
-        }
-      } else {
-        if ('+-×÷'.indexOf(buttonVal) > -1) {
-          // 如果n1和n2已被赋值，那么先计算之前n1和n2的结果，再记录新的操作符，并把n2清空
-          if (n1 && n2) {
-            result = getResult(n1, n2, operator)
-            outputSpan.textContent = result
-            n1 = result
-          }
-          n2 = ''             // 清空n2以存储新值
-          stepNum = ''        // 清空逐级计算的stepNum
-          prevOperator = ''   // 清空先前的操作符
-          operator = buttonVal
-        } else if (buttonVal === '=') {
-          n1 = n1 || result
-          n2 = n2 || stepNum || n1
-          operator = operator || prevOperator
-          result = getResult(n1, n2, operator)
-          outputSpan.textContent = result
-          n1 = result
-          stepNum = n2
-          prevOperator = operator
-          operator = ''
-          n2 = ''
-        } else if (buttonVal === '+/-') {
-          if (n2) {
-            n2 = getOppositeNumString(n2)
-            outputSpan.textContent = n2
-          } else if (n1) {
-            n1 = getOppositeNumString(n1)
-            outputSpan.textContent = n1
+  listenButtonsEvent() {
+    this.calculatorBox.addEventListener('click', (e) => {
+      if (e.target instanceof HTMLButtonElement) {
+        const buttonVal = e.target.textContent
+        if ('1234567890.'.indexOf(buttonVal) > -1) {
+          // 当按数字键时，如果操作符存在，那么一定是对n2进行赋值
+          if (this.operator) {
+            this.n2 = this.getNewNumString(this.n2, buttonVal)
+            this.outputSpan.textContent = this.n2
+          } else {
+            this.n1 = this.getNewNumString(this.n1, buttonVal)
+            this.outputSpan.textContent = this.n1
           }
         } else {
-          // clear
-          n1 = ''
-          n2 = ''
-          operator = ''
-          result = '0'
-          outputSpan.textContent = result
+          if ('+-×÷'.indexOf(buttonVal) > -1) {
+            // 如果n1和n2已被赋值，那么先计算之前n1和n2的结果，再记录新的操作符，并把n2清空
+            if (this.n1 && this.n2) {
+              this.result = this.getResult(this.n1, this.n2, this.operator)
+              this.outputSpan.textContent = this.result
+              this.n1 = this.result
+            }
+            this.n2 = ''             // 清空n2以存储新值
+            this.stepNum = ''        // 清空逐级计算的stepNum
+            this.prevOperator = ''   // 清空先前的操作符
+            this.operator = buttonVal
+          } else if (buttonVal === '=') {
+            this.n1 = this.n1 || this.result
+            this.n2 = this.n2 || this.stepNum || this.n1
+            this.operator = this.operator || this.prevOperator
+            this.result = this.getResult(this.n1, this.n2, this.operator)
+            this.outputSpan.textContent = this.result
+            this.n1 = this.result
+            this.stepNum = this.n2
+            this.prevOperator = this.operator
+            this.operator = ''
+            this.n2 = ''
+          } else if (buttonVal === '+/-') {
+            if (this.n2) {
+              this.n2 = this.getOppositeNumString(this.n2)
+              this.outputSpan.textContent = this.n2
+            } else if (this.n1) {
+              this.n1 = this.getOppositeNumString(this.n1)
+              this.outputSpan.textContent = this.n1
+            }
+          } else {
+            // clear
+            this.n1 = ''
+            this.n2 = ''
+            this.operator = ''
+            this.result = '0'
+            this.outputSpan.textContent = this.result
+          }
         }
       }
-    }
-  })
-
-  function getNewNumString(originVal, inputVal) {
+    })
+  }
+  getNewNumString(originVal, inputVal) {
     let result = (!(inputVal === '.' && originVal.match(/\./g))) ? originVal + inputVal : originVal
     return result === '.' ? '0.' : result
   }
-  function getOppositeNumString(originVal) {
+  getOppositeNumString(originVal) {
     return originVal.indexOf('-') > -1 ? originVal.slice(1) : '-' + originVal
   }
 
-  function getResult(n1, n2, operator) {
+  getResult(n1, n2, operator) {
     const num1 = parseFloat(n1) || 0, num2 = parseFloat(n2) || 0
     if (operator === '+') {
       return String(num1 + num2)
@@ -118,3 +132,5 @@
     return n1
   }
 }
+
+new Calculator()
